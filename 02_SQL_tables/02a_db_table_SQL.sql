13ef5963320b21e3350f1ab72b8638e4f29c3d3b
Dales92 <dlesnik23@gmail.com>
2022-05-03 09:48:06 +0000

    Befehle, Tabelle angelegt ++

--- 02_SQL_tables/02a_db_table_SQL.sql
+++ 02_SQL_tables/02a_db_table_SQL.sql
@@ -6,7 +6,34 @@
 /* Datenbanken auf Serveranzeigen */
 SHOW DATABASES;
 
+/* DB Boo löschen, falls vorhanden*/
+DROP DATABASE IF EXISTS boo;
+
 /* DB Boo ANLEGEN; FALLS NOCH NICHT VORHANDEN*/
 CREATE DATABASE IF NOT EXISTS boo;
 
 SHOW DATABASES;
+
+/* DB auswählen */
+USE boo;
+
+CREATE TABLE test
+(
+    name VARCHAR(20),
+    age INT
+);
+
+
+/* Alles Tabellen in der DB anzeigen */
+SHOW TABLES;
+
+/* Struktur der Tabelle anzeigen */
+DESCRIBE test;
+
+/* ----- Daten ----- */
+INSERT INTO test(name,age) VALUES ("Grizabella", 29);
+INSERT INTO test(age,name) VALUES (35, "Alonzo");
+INSERT INTO test VALUES();
+
+/* ----- Inhalte der Tabelle anzeigen ----- */
+SELECT * FROM test;
